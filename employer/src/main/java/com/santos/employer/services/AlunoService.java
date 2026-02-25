13a84e6506c836185b69997916e49f0511e12572
Johan Gabriel <johan.universidade@gmail.com>
2026-02-24 22:00:55 -0300

    servico criado

---
+++ employer/src/main/java/com/santos/employer/services/AlunoService.java
@@ -0,0 +1,36 @@
+package com.santos.employer.services;
+
+
+import com.santos.employer.models.AlunoModel;
+import com.santos.employer.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public AlunoModel criarAluno(AlunoModel alunoModel){
+        return alunoRepository.save(alunoModel);
+    }
+
+
+    public List<AlunoModel> findAll(){
+        return alunoRepository.findAll();
+    }
+
+
+    public void deletarAluno(Long id){
+        alunoRepository.deleteAllById(id);
+    }
+
+
+
+
+
+
+}
