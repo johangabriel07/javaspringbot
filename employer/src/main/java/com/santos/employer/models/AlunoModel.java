663b5e6bab4339450306bd92cf8e1ac8df4e05db
Johan Gabriel <johan.universidade@gmail.com>
2026-02-24 21:39:50 -0300

    model criada

---
+++ employer/src/main/java/com/santos/employer/models/AlunoModel.java
@@ -0,0 +1,54 @@
+package com.santos.employer.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TBL_ALUNO")
+public class AlunoModel {
+
+    @Id //cria a chave primaria no banco
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // gera ID automaticamente
+    private Long id;
+    private String nome;
+    private String matricula;
+    private String endereco;
+
+
+    public AlunoModel(){
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
